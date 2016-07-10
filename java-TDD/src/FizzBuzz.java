a6fda27c9bb3babee5d78821c9a1f870824a24c4
Zhanglei-git <542013793@qq.com>
2016-07-10 13:44:40 +0800

    creat FizzBuzz.java & TestFizzBuzz.java

---
+++ java-TDD/src/FizzBuzz.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ZL on 2016/7/10.
+ */
+public class FizzBuzz {
+    public String work(Integer inputNumber){
+        return "";
+    }
+}
---
+++ java-TDD/test/TestFizzBuzz.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+/**
+ * Created by ZL on 2016/7/10.
+ */
+public class TestFizzBuzz {
+    FizzBuzz fb;
+    @Before
+    public void setUp() throws Exception {
+        fb = new FizzBuzz();
+    }
+}
