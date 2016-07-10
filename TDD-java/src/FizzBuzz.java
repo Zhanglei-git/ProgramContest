37c19f70bdea6a734e7a6ac74e47fd14f98b31a6
preservance717 <GL717@outlook.com>
2016-07-10 21:12:34 +0800

    init

---
+++ TDD-java/src/FizzBuzz.java
@@ -0,0 +1,10 @@
+/**
+ * Created by gaole on 7/10/16.
+ */
+public class FizzBuzz {
+    public FizzBuzz() {
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ TDD-java/test/TestFizzBuzz.java
@@ -0,0 +1,5 @@
+/**
+ * Created by gaole on 7/10/16.
+ */
+public class TestFizzBuzz {
+}
