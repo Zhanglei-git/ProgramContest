d21c70b265c03096f397252592d97e8453898aa5
preservance717 <GL717@outlook.com>
2016-07-14 17:23:37 +0800

    return FIZZ when inputNum is 3

--- TDD-java/src/FizzBuzz.java
+++ TDD-java/src/FizzBuzz.java
@@ -2,9 +2,12 @@
  * Created by gaole on 7/10/16.
  */
 public class FizzBuzz {
-    public FizzBuzz() {
-    }
+    public String work(Object input) {
+        int inputNum = (Integer) input;
 
-    public static void main(String[] args) {
+        if (inputNum % 3 == 0) {
+            return "FIZZ";
+        }
+        return "";
     }
 }
--- TDD-java/test/TestFizzBuzz.java
+++ TDD-java/test/TestFizzBuzz.java
@@ -1,5 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by gaole on 7/10/16.
  */
 public class TestFizzBuzz {
+    FizzBuzz fb;
+
+    @Before
+    public void setup() throws Exception{
+        fb = new FizzBuzz();
+    }
+
+    @Test
+    public void return_Fizz_when_inputNumber_is_3() throws Exception{
+          assertEquals("FIZZ", fb.work(3));
+    }
 }
